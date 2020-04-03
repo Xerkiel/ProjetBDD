82a712c1a41da85eb186e69ff3a90161e85efc94
Victor Thuillié <garrosr78@yahoo.fr>
2020-04-03 12:31:40 +0200

    Lancement du git

---
+++ ProjetBDD/ProjetBDD/Connection.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ProjetBDD
+{
+    /// <summary>
+    /// Logique d'interaction pour Connection.xaml
+    /// </summary>
+    public partial class Connection : Page
+    {
+        public Connection()
+        {
+            InitializeComponent();
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            this.NavigationService.Navigate(new Inscription());
+        }
+    }
+}
