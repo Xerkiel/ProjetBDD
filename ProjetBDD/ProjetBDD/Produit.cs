c423fc20646d6379a15c5a55c01ba1978be87f68
Hugo <tenghugo92@gmail.com>
2020-04-27 13:46:25 +0200

    Auto stash before merge of "Hugo" and "origin/Hugo"

--- ProjetBDD/ProjetBDD/Produit.cs
+++ ProjetBDD/ProjetBDD/Produit.cs
@@ -8,5 +8,26 @@ namespace ProjetBDD
 {
     class Produit
     {
+        private int? Id { get; }
+        private string Nom { get; }
+        private string Categorie { get; }
+        private float UQA { get; }
+        private int Quantite { get; }
+        private int Quantite_Minimal { get; }
+        private int Quantite_Maximal { get; }
+        private string Fournisseur { get; }
+
+
+        public Produit(int id, string nom, string categorie, float UQA, int quantite, int quantite_minimal, int quantite_maximal, string fournisseur)
+        {
+            this.Id = id;
+            this.Nom = nom;
+            this.Categorie = categorie;
+            this.UQA = UQA;
+            this.Quantite = quantite;
+            this.Quantite_Minimal = quantite_minimal;
+            this.Quantite_Maximal = quantite_maximal;
+            this.Fournisseur = fournisseur;
+        }
     }
 }
